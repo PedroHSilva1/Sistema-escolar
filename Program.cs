17a6211115473c9c0f98ff50da9c75abf4ec2818
PedroHSilva1 <148078634+PedroHSilva1@users.noreply.github.com>
2023-11-28 23:36:02 -0300

    Versão 1.0
    Versão funcional do código!

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Sistema
 {
+    
     internal abstract class Cadastro
     {
         public string nome { get; set; }
@@ -152,12 +153,217 @@ namespace Sistema
             {
                 Console.WriteLine("\nCadastro Realizado!");
             }
-            
+
         }
 
     }
 
-    class LoginWIP
+    public abstract class Retorno
+    {
+        public int escolha;
+        public string confirma;
+
+        public abstract void Usuario();
+
+        public void Retornar()
+        {
+            Console.WriteLine("\nDeseja acessar outra aba?(s/n)");
+            confirma = Console.ReadLine();
+            if (confirma == "s" || confirma == "S")
+            {
+                Usuario();
+            }
+            if (confirma == "n" || confirma == "N")
+            {
+                Sair();
+            }
+
+        }
+        public void Sair()
+        {
+            Console.Clear();
+            Console.WriteLine("\nO sistema irá ser encerrado. Tem certeza que quer encerrar?(s/n");
+            confirma = Console.ReadLine();
+            if (confirma == "s" || confirma == "S")
+            {
+                Console.WriteLine("\nAté mais!");
+                Environment.Exit(0);
+            }
+            else if (confirma == "n" || confirma == "N")
+            {
+                Usuario();
+            }
+            else 
+            {
+                Console.Clear();
+                Console.WriteLine("Opção inválida");
+                Sair();
+                
+            }
+            
+
+        }
+    }
+
+    class Aluno:Retorno
+    {
+
+        public override void Usuario() {
+            
+            
+            Console.Clear();
+            Console.WriteLine("Boas vindas ao portal do aluno!");
+            Console.WriteLine("Selecione a opção para a função desejada:");
+            Console.WriteLine("\n1- Verificar suas notas.\n2- Verificar calendário de presenças.\n3-Ver Observações.\n4-Encerrar sistema.");
+            escolha = Int32.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    Materias materias01 = new Materias("");
+                    materias01.Demonstrar();
+                    Retornar();
+
+                    break;
+                case 2:
+                    Presenca presenca01 = new Presenca(1);
+                    presenca01.demo();
+                    Retornar();
+                    
+                    break;
+                case 3:
+                    Observador observador01 = new Observador();
+                    observador01.VerObserv();
+                    Retornar();
+                    break;
+
+                case 4:
+                    Sair();
+                    break;
+                default:
+                    Usuario();
+                    break;
+            }
+
+        }
+
+    }
+
+    class Professor:Retorno
+    {
+        public override void Usuario()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Boas vindas ao portal do professor!");
+            Console.WriteLine("Selecione a opção para a função desejada:");
+            Console.WriteLine("\n1- Lançar notas.\n2- Adicionar falta ou presença.\n3-lançar Observações.\n4-Encerrar sistema.");
+            escolha = Int32.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    Materias materias01 = new Materias("");
+                    materias01.AddNota();
+                    Retornar();
+                    
+                    break;
+                case 2:
+                    Presenca presenca01 = new Presenca(1);
+                    Console.Clear();
+                    Console.WriteLine("Presença");
+                    Console.WriteLine("\nSelecione o que você deseja\n\n1- Adicionar presença.\n2- Adicionar falta.\n3- Adicionar falta justificada.\n4- Encerrar sistema");
+                    escolha = Int32.Parse(Console.ReadLine());
+
+                    switch (escolha)
+                    {
+                        case 1:
+                            presenca01.MarcarPresencaM();
+                            Retornar();
+                            break;
+                        case 2:
+                            presenca01.MarcarFaltaM();
+                            Retornar();
+                            break;
+                        case 3:
+                            presenca01.MarcarEspecialM();
+                            Retornar();
+                            break;
+                        case 4:
+                            Sair();
+                            break;
+                        default:
+                            Console.WriteLine("Opçao invalida");
+                            Retornar();
+                            break;
+                    }
+                    Retornar();
+
+                    break;
+                case 3:
+                    Observador observador01 = new Observador();
+                    observador01.AddObserv();
+                    Retornar();
+                    break;
+                case 4:
+                    Sair();
+                    break;
+                default:
+                    Usuario();
+                    break;
+            }
+
+        }
+
+
+    }
+
+    class Coordenador : Retorno
+    {
+
+        public override void Usuario()
+        {
+
+
+            Console.Clear();
+            Console.WriteLine("Boas vindas ao portal do Coordenador!");
+            Console.WriteLine("Selecione a opção para a função desejada:");
+            Console.WriteLine("\n1- Cadastrar aluno.\n2- Cadastrar Funcionario.\n3-Lançar Observações.\n4-Encerrar sistema.");
+            escolha = Int32.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    CadastroAluno cadastroAluno = new CadastroAluno();
+                    cadastroAluno.Cadastrar();
+                    Retornar();
+
+                    break;
+                case 2:
+                    CadastroFuncionario cadastroFuncionario = new CadastroFuncionario();
+                    cadastroFuncionario.Cadastrar();
+                    Retornar();
+
+                    break;
+                case 3:
+                    Observador observador01 = new Observador();
+                    observador01.AddObserv();
+                    Retornar();
+                    break;
+
+                case 4:
+                    Sair();
+                    break;
+                default:
+                    Usuario();
+                    break;
+            }
+
+        }
+
+    }
+
+    class Login
     {
         private string nomeAluno = "aluno";
         private string senhaAluno = "111";
@@ -180,27 +386,34 @@ namespace Sistema
             {
                 Console.Clear();
                 Console.WriteLine("Boas vindas " + nomeAluno);
-                /* aqui haverá VerNotas;;, Ver Calendario, VerObservacoes, VerPresenças;;*/
-            }else 
+                Aluno aluno01 = new Aluno();
+                aluno01.Usuario();
+            }
+            else
             if (confUser == nomeCoor && confPassw == senhaCoor)
             {
                 Console.Clear();
                 Console.WriteLine("Boas vindas Coordenador(a)");
-                /* aq haverá Cadastro de alunos e funcionarios;;, LançarObservacoes, AjustarCalendario*/
-            }else
+                Coordenador coordenador01 = new Coordenador();
+                coordenador01.Usuario();
+                
+            }
+            else
             if (confUser == nomeProf && confPassw == senhaProf)
             {
-                
+
                 Console.Clear();
                 Console.WriteLine("Boas vindas Professor(a)");
-                /*Aq haverá MarcarPresença;;, LançarObservacoes,AdcionarNota;;*/
+                Professor professor01 = new Professor();
+                professor01.Usuario();
+
             }
-             else
-             {
+            else
+            {
                 Console.Clear();
                 Console.WriteLine("Login ou senha incorretos, Tente novamente.\n");
                 Logar();
-             }
+            }
 
         }
 
@@ -221,30 +434,40 @@ namespace Sistema
         public void VerNotas()
         {
             Console.Clear();
+
+            Console.WriteLine("Notas de " + Nome + ":");
             
-            Console.WriteLine("Notas de "+Nome+":");
-            /*trocar semestre para unidade*/
-            for (int semestre = 0; semestre < 2; semestre++)
+            for (int unidade = 0; unidade < 2; unidade++)
             {
-                Console.WriteLine("Semestre " + (semestre + 1) + ":");
-                for (int tipoNota = 0; tipoNota <3; tipoNota++)
+                Console.WriteLine("Unidade " + (unidade + 1) + ":");
+                for (int tipoNota = 0; tipoNota < 3; tipoNota++)
                 {
                     string nomeNota = PegarNomeNota(tipoNota);
-                    Console.WriteLine($"{nomeNota}: {Notas[semestre, tipoNota]}");
+                    Console.WriteLine($"{nomeNota}: {Notas[unidade, tipoNota]}");
                 }
                 Console.WriteLine();
             }
 
         }
 
-        public void AddNota(int semestre,int tipoNota,double nota)
+        public void AddNota()
         {
-            if (semestre <1||semestre>2 ||tipoNota<1 ||tipoNota>3)
+            /*Adicionar um DO WHILE para perguntar por mais notas*/
+            Console.Clear();
+            Console.WriteLine("\nIncrementador de nota");
+            Console.WriteLine("\nDigite a unidade desejada: (1 ou 2)");
+            int unidade = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Selecione o tipo de nota: \n\n1-Teste\n2-Trabalho\nProva");
+            int tipoNota = Int32.Parse(Console.ReadLine());
+            
+            if (unidade < 1 || unidade > 2 || tipoNota < 1 || tipoNota > 3)
             {
-                Console.WriteLine("Semestre ou tipo de nota inválido");
-                return;
+                Console.WriteLine("Unidade ou tipo de nota inválido");
+                AddNota();
             }
-            Notas[semestre - 1, tipoNota - 1] = nota;
+            Console.WriteLine("Insira a nota:");
+            double nota = Int32.Parse(Console.ReadLine());
+            Notas[unidade - 1, tipoNota - 1] = nota;
         }
 
         private string PegarNomeNota(int tipoNota)
@@ -265,16 +488,17 @@ namespace Sistema
         public void Demonstrar()
         {
             Materias matematica = new Materias("matemática");
-            matematica.AddNota(1, 1, 5.5);
-            matematica.AddNota(1, 2, 7.5);
-            matematica.AddNota(1, 3, 10);
-            matematica.AddNota(2, 1, 8.5);
-            matematica.AddNota(2, 2, 3.5);
-            matematica.AddNota(2,3, 10);
+            matematica.Notas[0, 0] = 5.5; 
+            matematica.Notas[0, 1] = 7.5; 
+            matematica.Notas[0, 2] = 10.0; 
+
+            matematica.Notas[1, 0] = 8.5; 
+            matematica.Notas[1, 1] = 3.5; 
+            matematica.Notas[1, 2] = 10.0; 
 
             matematica.VerNotas();
 
-             
+
         }
 
 
@@ -282,7 +506,7 @@ namespace Sistema
 
     class Presenca
     {
-        private int[,] matrizPresenca; /*1- presente, 2- falta, 3-especial*/
+        private int[,] matrizPresenca; 
         private int diasUteis;
 
         public Presenca(int diasUteis)
@@ -295,13 +519,35 @@ namespace Sistema
         {
             MarcarPouF(dia, 1);
         }
+
+        public void MarcarPresencaM()
+        {
+            Console.WriteLine("Insira o dia da presença:");
+            int dia = Int32.Parse(Console.ReadLine());
+            MarcarPresenca(dia);
+        }
+
         public void MarcarFalta(int dia)
         {
             MarcarPouF(dia, 2);
         }
+        public void MarcarFaltaM()
+        {
+            Console.WriteLine("Insira o dia da falta:");
+            int dia = Int32.Parse(Console.ReadLine());
+            MarcarFalta(dia);
+        }
+
         public void MarcarEspecial(int dia)
         {
             MarcarPouF(dia, 3);
+        }
+
+        public void MarcarEspecialM()
+        {
+            Console.WriteLine("Insira o dia da falta justificada:");
+            int dia = Int32.Parse(Console.ReadLine());
+            MarcarEspecial(dia);
         }
 
         private void MarcarPouF(int dia, int tipo)
@@ -340,7 +586,7 @@ namespace Sistema
             }
 
             int ausencias = totalFalta - totalEspecial;
-            double porcentagemPresenca = (double)totalPresenca / (diasUteis - ausencias ) * 100;
+            double porcentagemPresenca = (double)totalPresenca / (diasUteis - ausencias) * 100;
 
             Console.WriteLine($"\nTotal de presenças: {totalPresenca}");
             Console.WriteLine($"Total de faltas: {totalFalta}");
@@ -351,13 +597,13 @@ namespace Sistema
 
         public void ListasDeFaltas()
         {
-            Console.WriteLine("\nDias de falta:");
+            Console.WriteLine("\nDias de falta sem justificativa:");
 
             for (int dia = 0; dia < diasUteis; dia++)
             {
-                if (matrizPresenca[dia,1]>0)
+                if (matrizPresenca[dia, 1] > 0)
                 {
-                    Console.WriteLine($"dia {dia+1}");
+                    Console.WriteLine($"dia {dia + 1}");
                 }
             }
         }
@@ -381,12 +627,55 @@ namespace Sistema
 
     }
 
+    class Observador
+    {
+        private List<string> observacoes = new List<string>(); 
+        
+        public void VerObserv()
+        {
+            Console.WriteLine("\nObservações:");
+
+            if (observacoes.Count == 0)
+            {
+                Console.WriteLine("Nenhuma observação disponível.");
+            }
+            else
+            {
+                for (int i = 0; i < observacoes.Count; i++)
+                {
+                    Console.WriteLine($"{i+1}: {observacoes[i]}");
+                }
+            }
+            
+        }
+
+        public void AddObserv()
+        {
+            Console.WriteLine("\nAdicionar Observação:");
+            Console.WriteLine("Digite a Observação:");
+
+            string novaObservacao = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(novaObservacao))
+            {
+                observacoes.Add(novaObservacao);
+                Console.WriteLine("\nObservação adicionada com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Observação inválida. Não foi adicionada!");
+            }
+        }
+
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Presenca presenca = new Presenca(20);
-            presenca.demo();
+            Login login = new Login();
+            login.Logar();
         }
     }
 }
